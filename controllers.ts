f081d1621d63855cbd084457083315d69b80f929
rksquared <rahul@rahulkrish.com>
2018-05-15 19:15:38 -0400

    removed medium, updated formatting and result limits on endpoints

--- controllers.ts
+++ controllers.ts
@@ -1,12 +1,10 @@
 import { Router } from 'express';
 import { wikiScraper, wikiRecommendations } from './scrapers/wiki';
-import { getPic, scrapeMedium } from './scrapers/medium';
 import { getSearchResults } from './wikipediaAPI/search';
 
 const router: Router = Router();
 
 router.post('/api/wiki', wikiScraper);
-router.post('/api/medium', scrapeMedium);
 router.post('/api/wikiSearch', getSearchResults);
 router.post('/api/wikiRecommendations', wikiRecommendations);
 
--- wikipediaAPI/search.ts
+++ wikipediaAPI/search.ts
@@ -4,7 +4,7 @@ const getSearchResults = (req: any, res: any) => {
   const { query } = req.body;
   axios.get(`https://en.wikipedia.org/w/api.php?action=opensearch&search=${query}&limit=10&namespace=articles&format=json`)
   .then((result: any) => {
-    res.send(result.data[1]);
+    res.send(result.data[1].map((id: string) => ({ id, label: id })));
   })
   .catch((err: any) => {
     console.log(err);
