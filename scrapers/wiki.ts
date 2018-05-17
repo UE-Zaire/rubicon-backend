f5ae5a5408c554bdb915c0802b2329c252e697a6
Pedro Barquinha <barquinha.pedro@gmail.com>
2018-05-17 15:12:47 -0400

    improved wiki and web recommendations

--- scrapers/wiki.ts
+++ scrapers/wiki.ts
@@ -6,30 +6,49 @@ interface Graph {
   nodes: Array<{ id: string | undefined; group: number | undefined; }>
 }
 
+const getWikiSearchResults = (req: any, res: any) => {
+  const { query } = req.body;
+  axios.get(`https://en.wikipedia.org/w/api.php?action=opensearch&search=${query}&limit=10&namespace=articles&format=json`)
+  .then((result: any) => {
+    res.send(result.data[1].map((id: string) => ({ id, label: id })));
+  })
+  .catch((err: any) => {
+    console.log(err);
+  })
+}
+
 const wikiRecommendations = (req: any, res: any) => {
   const { link, query } = req.body;
+  const memo: any = {};
+
   axios.get(link)
   .then((result: any) => {
-    const memo: any = {};
-    const $ = cheerio.load(result.data);
-    const arr: any = $('a', '#bodyContent');
-    const arrH: any = $('a', '#bodyContent').html();
-    const recommendations: Graph = {links: [], nodes: []};
-    recommendations.nodes.push({id: query, group: 1});
+   const recommendations: Graph = {
+     links: [], 
+     nodes: [ 
+     {id: query, group: 1} 
+     ]
+   };
 
-    for (let i = 0; recommendations.nodes.length < arr.length && recommendations.nodes.length < 100; i++) {
-      if (arr[i].children[0]) {
-        const item: any = arr[i].children[0].data;
-        if (item && !item.match(/\W/)) {
+   const $ = cheerio.load(result.data);
+   var sent = false;
 
-          recommendations.nodes.push({id: item, group: 1});
-          recommendations.links.push({source: query, target: item, value: 1});
-          memo[item] = true;
-        }
-      }
-    }
-    
-    res.send(recommendations);
+   $('div[id=content]').find('a').each((i, ele) => {
+     const title = $(ele).attr('title');
+
+     if (title && !title.match(/\W/) && !memo[title]) {
+       recommendations.nodes.push({id: title, group: 1});
+       recommendations.links.push({source: query, target: title, value: 1});
+       memo[title] = true;
+     } 
+
+     if (recommendations.nodes.length === 100 && !sent) {
+       sent = true;
+       res.send(recommendations);
+     }
+   })
+
+   if (!sent) res.send(recommendations);
   })
   .catch((err: any) => {
     console.log(err);
@@ -38,5 +57,6 @@ const wikiRecommendations = (req: any, res: any) => {
 
 export {
   wikiRecommendations,
+  getWikiSearchResults,
   Graph
 }
