c16fabbc3f4abedd69497545556235fd2e9e9277
Pedro Barquinha <barquinha.pedro@gmail.com>
2018-05-21 10:57:13 -0400

    added link property to each node in webRecommendations, and deployed database.

--- socketHandlers.ts
+++ socketHandlers.ts
@@ -3,10 +3,10 @@ import { socket } from './index';
 const handleSocketConnections = (socket: socket.Socket) => {
 
   console.log('user connected');
-
+  
   socket.on('historyForExtension', (data) => {
     console.log(data);
-    setTimeout(() => socket.emit('graphData', 'hello heres a graph'), 1000);
+    socket.emit('graphData', data);
   });
 
   socket.on('ext', (data) => {
