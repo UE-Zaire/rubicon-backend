cad0991973d4ff1ce7000ab476af0e209662b0f2
Pedro Barquinha <barquinha.pedro@gmail.com>
2018-05-21 19:55:53 -0400

    added saving and retrieving all histories

--- database/controllers/users.ts
+++ database/controllers/users.ts
@@ -1,12 +1,13 @@
 import knex from '../database';
 
 const addUser = async (id: string, displayName: string) => {  
-  await knex('users').insert({ user_name: displayName, email: id });
+  await knex('users').insert({ user_name: displayName, email_id: id });
 };
 
 const login = async (token: any, refreshToken: any, profile: any, done: any) => {
   const { id, displayName } = profile;  
-  const checkUser = await knex('users').select().where({ user_name: displayName, email: id });
+  console.log(id, displayName)
+  const checkUser = await knex('users').select().where({ user_name: displayName, email_id: id });
 
   if (!checkUser.length) {
     addUser(id, displayName);
@@ -19,5 +20,6 @@ const login = async (token: any, refreshToken: any, profile: any, done: any) => 
 };
 
 export {
-  login
+  login,
+  addUser
 }
